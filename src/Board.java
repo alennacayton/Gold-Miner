041e54a92ff851208354b57a206929ccc23d1a2f
alenna <rainiiicorn@gmail.com>
2020-11-30 02:05:12 +0800

    updated version - added gui

--- src/Board.java
+++ src/Board.java
@@ -1,36 +1,47 @@
 public class Board {
 
-    private Spot [][] boxes;
+    private Tile [][] boxes;
     private int size;
 
-    public Board(int s)
+    public Board(int s, int unitSize)
     {
         this.size = s;
-        this.boxes = new Spot [s][s];
+        this.boxes = new Tile [s][s];
+
+
+        int x = 0;
+        int y = 0;
 
         for(int i = 0; i < s; i++)
         {
-            for(int j = 0; j < s; j++)
-            {
+                for(int j = 0; j < s; j++)
+                {
 
-                boxes[i][j] = new Spot (i,j);
-                boxes[i][j].setState('*');
+                        boxes[i][j] = new Tile (x,y);
+                        boxes[i][j].setState('*');
 
 
+                    x += unitSize;
 
-            }
+                }
+
+            y += unitSize;
+            x = 0;
+
+
         }
+
+
+
 
         for(int a = 0; a < s; a++)
         {
             for(int b = 0; b < s; b++)
             {
-                System.out.print(boxes[a][b].getState() + " ");
+                System.out.print(boxes[a][b].getState() + "  ");
 
                 if (b == s - 1)
                     System.out.println();
-
-
 
             }
         }
@@ -42,10 +53,11 @@ public class Board {
     }
 
 
-    public Spot getBox (int x, int y)
+    public Tile getBox (int row, int col)
     {
-        return boxes [x][y];
+        return boxes [row][col];
     }
+
 
     public void displayBoard ()
     {
@@ -62,6 +74,24 @@ public class Board {
 
             }
         }
+
+    int count = 0;
+        for(int a = 0; a < this.size; a++)
+        {
+            for(int b = 0; b < this.size; b++)
+            {
+                    count++;
+
+                System.out.println(a + "    " + b);
+
+                System.out.println(" X pos = " + boxes[a][b].getX() + " Y pos = " + boxes[a][b].getY());
+
+
+
+            }
+        }
+
+        System.out.println("COUNT " + count);
 
 
     }
--- src/GoldMiner.java
+++ src/GoldMiner.java
@@ -1,62 +1,72 @@
-import java.util.Scanner;
-
 public class GoldMiner {
 
+    static public void openGameMenu(){
+
+        GameMenu game = new GameMenu();
+
+    }
+
+    static public void importFiles(){
+
+/*
+            File inputFile;
+            Account c;
+            GovernmentOfficial g;
+            ContactTracer t;
 
 
+            try {
+                inputFile = new File("coordinates.txt");
+                Scanner reader = new Scanner(inputFile);
 
-    public static void main (String args[])
+                while (reader.hasNext()) {
+
+                    String username = reader.next();
+                    String type = reader.next();
+
+                    if (type.equals("citizen"))
+                    {
+                        c = new Account (username, "123456");
+                        acclist.add(c);
+                        reader.nextLine();
+                    }
+
+                    else if (type.equals("official"))
+                    {
+                        g = new GovernmentOfficial (username, "123456");
+                        acclist.add(g);
+                        reader.nextLine();
+                    }
+
+                    else if (type.equals("tracer"))
+                    {
+                        t = new ContactTracer(username, "123456");
+                        acclist.add(t);
+                        reader.nextLine();
+                    }
+
+                }
+                reader.close();
+
+            }
+
+            catch (FileNotFoundException ex) {
+                System.out.println("File does not exist!");
+                System.exit(1);
+            }
+
+*/
+
+    }
+
+
+    public static void main (String [] args)
     {
-        Scanner sc = new Scanner(System.in);
 
-        Miner player = new Miner();
-        int nSize, nB, nP;
-        System.out.print("Enter size of board: ");
-        nSize = sc.nextInt();
+        importFiles();
+        openGameMenu();
 
-        Board board = new Board(nSize); // initializing board
-        sc.nextLine();
-        System.out.print("Enter coordinates of pot of gold (x,y): ");
-        String [] temp = sc.nextLine().split(",", 2);
-
-        board.getBox(Integer.parseInt(temp[0]), Integer.parseInt(temp[1])).setState('G');
-
-        System.out.print("Number of BEACONS: ");
-        nB = sc.nextInt();
-
-        sc.nextLine();
-
-        System.out.println("Enter coordinates of beacons (x,y): ");
-
-        for(int i = 0; i < nB; i++)
-        {
-            String [] beacons = sc.nextLine().split(",", 2);
-            board.getBox(Integer.parseInt(beacons[0]), Integer.parseInt(beacons[1])).setState('B');
-
-        }
-
-        System.out.print("Number of PITS: ");
-        nP = sc.nextInt();
-
-        sc.nextLine();
-
-        System.out.println("Enter coordinates of pits (x,y): ");
-
-        for(int i = 0; i < nP; i++)
-        {
-            String [] pits = sc.nextLine().split(",", 2);
-            board.getBox(Integer.parseInt(pits[0]), Integer.parseInt(pits[1])).setState('P');
-
-        }
-
-
-
-
-
-        board.displayBoard();
-
-
-
+      //  GameFrame frame = new GameFrame();
 
     }
 }
